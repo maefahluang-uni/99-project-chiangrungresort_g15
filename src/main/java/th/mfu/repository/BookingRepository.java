f8cdafe32f8ae59540e820ed6b6cca75b94b4065
6531503011 <6531503011@lamduan.mfu.ac.th>
2023-11-26 12:29:28 +0800

    6531503011

--- src/main/java/th/mfu/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package th.mfu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import th.mfu.entity.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-    
-}

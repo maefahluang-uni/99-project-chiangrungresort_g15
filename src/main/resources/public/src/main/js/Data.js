100157f8aa14d53620e31dd593a40f9d4e220aa9
Jomkwan_Nimphung <122781989+6531503013@users.noreply.github.com>
2023-11-26 12:39:51 +0000

    6531503013

---
+++ src/main/resources/public/src/main/js/Data.js
@@ -0,0 +1,31 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Sweet Basil",
+      price: 65,
+      desc: "Meat, basil leaves, chili, garlic, fried egg.",
+      img:src="src/main/images/img-1.jpg"
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Breakfast",
+      price: 60,
+      desc: "Sausage, fried egg, bread.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Chocolate Mint",
+      price: 55,
+      desc: "Chocolate ,Mint.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Cocktaile",
+      price: 90,
+      desc: "Alcohol,Fruit.",
+      img: "images/img-4.jpg",
+    },
+  ];
+  

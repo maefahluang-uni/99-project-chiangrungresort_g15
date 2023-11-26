bdac4f757cd06cd3c2283b8e9039d5e5fc35c94b
Kittikanning <122779463+Kittikanning@users.noreply.github.com>
2023-11-26 14:44:17 +0000

    6531503009

---
+++ src/main/resources/public/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Sweet Basil",
+    price: 65,
+    desc: "Meat, basil leaves, chili, garlic, fried egg.",
+    img: src="src/main/images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Breakfast",
+    price: 60,
+    desc: "Sausage, fried egg, bread.",
+    img: src="src/main/images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Chocolate Mint",
+    price: 55,
+    desc: "Chocolate ,Mint.",
+    img: src="src/main/images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Cocktaile",
+    price: 90,
+    desc: "Alcohol,Fruit.",
+    img:src="src/main/images/img-4.jpg",
+  },
+];
